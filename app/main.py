13ed9af230d3a77aa6557c293cc5fc26c86d9feb
Adarsh Tiwari <Adarsh.tiwari@yash.com>
2021-06-11 15:29:14 +0530

    new update

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ app = FastAPI(title='Serverless Lambda FastAPI')
 app.include_router(api_router, prefix="/api/v1")
 
 
+
 @app.get("/",  tags=["Endpoint Test"])
 def main_endpoint_test():
     return {"message": "Welcome CI/CD Pipeline with GitHub Actions!"}
