16fb40d50ff4770351320b8cb899ae3c03ce11bc
Adarsh Tiwari <Adarsh.tiwari@yash.com>
2021-06-15 15:25:59 +0530

    add mongo dependency

--- app/main.py
+++ app/main.py
@@ -13,26 +13,26 @@ def main_endpoint_test():
     return {"message": "Welcome CI/CD Pipeline with GitHub Actions!"}
 
 
-@app.get("/",  tags=["Posts"])
+@app.get("/user",  tags=["Posts"])
 def main_endpoint_test2():
     return {"message": "Welcome CI/CD Pipeline with GitHub Actions Post!"}
 
 app.include_router(api_router, prefix="/api/v1")
 
-handler = Mangum(app)
+# handler = Mangum(app)
 
 # to make it work with Amazon Lambda, we create a handler object
 # handler = Mangum(app=app)
-# def handler(event, context):
-#     event['requestContext'] = {}  # Adds a dummy field; mangum will process this fine
+def handler(event, context):
+    event['requestContext'] = {}  # Adds a dummy field; mangum will process this fine
     
-#     asgi_handler = Mangum(app)
-#     response = asgi_handler(event, context)
-#     json_data = event["queryStringParameters"] 
-#     # user = json_data["user"]
-#     return {
-#             "statusCode": 200,
-#             "headers": {"Content-Type": "application/json"},
-#             "body": json.dumps()
-#             }
+    asgi_handler = Mangum(app)
+    response = asgi_handler(event, context)
+    json_data = event["queryStringParameters"] 
+    # user = json_data["user"]
+    return {
+            "statusCode": 200,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps(response)
+            }
 
