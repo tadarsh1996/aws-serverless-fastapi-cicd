b6efcf257c4c39ca657e2afe2f9fb8dc1019bdd2
Adarsh Tiwari <Adarsh.tiwari@yash.com>
2021-06-16 18:26:12 +0530

    dwqds

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ from api.v1.api import router as api_router
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/",tags=["Endpoint Test"])
 async def root():
     return {"message": "Hello World!"}
 
@@ -20,8 +20,12 @@ async def root():
 def main_endpoint_test():
     return {"message": "Welcome CI/CD Pipeline with GitHub Actions Post!"}
 
+  
+
 app.include_router(api_router, prefix="/api/v1")
 handler = Mangum(app)
+
+
 
 # @app.get("/",  tags=["Endpoint Test"])
 # def main_endpoint_test():
@@ -38,16 +42,12 @@ handler = Mangum(app)
 
 # # to make it work with Amazon Lambda, we create a handler object
 # # handler = Mangum(app=app)
-# def handler(event, context):
-#     event['requestContext'] = {}  # Adds a dummy field; mangum will process this fine
+def handler(event, context):
+    event['requestContext'] = {}  # Adds a dummy field; mangum will process this fine
     
-#     asgi_handler = Mangum(app)
-#     response = asgi_handler(event, context)
-#     json_data = event["queryStringParameters"] 
-#     # user = json_data["user"]
-#     return {
-#             "statusCode": 200,
-#             "headers": {"Content-Type": "application/json"},
-#             "body": json.dumps("heeloaa")
-#             }
+    asgi_handler = Mangum(app)
+    response = asgi_handler(event, context)
+    json_data = event["queryStringParameters"] 
+    # user = json_data["user"]
+    return  response
 
