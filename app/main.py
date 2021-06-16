2c1be165e8bc26410917846260e3e904a1386872
Adarsh Tiwari <Adarsh.tiwari@yash.com>
2021-06-16 19:10:53 +0530

    saas

--- app/main.py
+++ app/main.py
@@ -2,17 +2,17 @@
 from fastapi import FastAPI, status
 from mangum import Mangum
 from pymongo import MongoClient
-from pydantic import BaseModel
+# from pydantic import BaseModel
 from typing import List
 import urllib.parse
 import json
 from api.v1.api import router as api_router
-password="root123"
-username="barkha"
-client = MongoClient(
-    "mongodb+srv://"+urllib.parse.quote_plus(username)+":"+urllib.parse.quote_plus(password)+"@cluster0.otmoo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
-)
-db = client.get_database('myFirstDatabase')
+# password="root123"
+# username="barkha"
+# client = MongoClient(
+#     "mongodb+srv://"+urllib.parse.quote_plus(username)+":"+urllib.parse.quote_plus(password)+"@cluster0.otmoo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
+# )
+# db = client.get_database('myFirstDatabase')
 
 
 app = FastAPI()
@@ -23,11 +23,11 @@ DB = "myFirstDatabase"
 COLLECTION = "users"
 
 # Message class defined in Pydantic
-class User(BaseModel):
-    first_name: str
-    last_name: str
-    email: str
-    mobile_number: int
+# class User(BaseModel):
+#     first_name: str
+#     last_name: str
+#     email: str
+#     mobile_number: int
 
 @app.get("/",  tags=["Endpoint Test"])
 def main_endpoint_test():
@@ -40,24 +40,24 @@ def handler(event, context):
     return response
 
 # adding user
-@app.post("/user", status_code=status.HTTP_201_CREATED)
-def post_message(user: User):
-        collection = db["users"] 
-        print(collection)
-        result = collection.insert_one(user.dict())
-        print(result)
-        return {"message":"Data inserted successfully"}
+# @app.post("/user", status_code=status.HTTP_201_CREATED)
+# def post_message(user: User):
+#         collection = db["users"] 
+#         print(collection)
+#         result = collection.insert_one(user.dict())
+#         print(result)
+#         return {"message":"Data inserted successfully"}
 
 # getting all users
-@app.get("/user", response_model=List[User])
-def get_users():
-        collection = db["users"] 
-        user_list = collection.find()
-        users=[]
-        for i in user_list:
-            users.append(User(**i))
-        print(users)
-        return users
+# @app.get("/user", response_model=List[User])
+# def get_users():
+#         collection = db["users"] 
+#         user_list = collection.find()
+#         users=[]
+#         for i in user_list:
+#             users.append(User(**i))
+#         print(users)
+#         return users
 
 # # getting single user by id
 # @app.get("/user/{user_id}", response_model=List[User])
