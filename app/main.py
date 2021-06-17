46e094f975f77c293ea785395b0dba2ae70e7c68
Adarsh Tiwari <Adarsh.tiwari@yash.com>
2021-06-17 13:11:21 +0530

    qaaz

--- app/main.py
+++ app/main.py
@@ -1,147 +1,37 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI
 from mangum import Mangum
-from pymongo import MongoClient
-from pydantic import BaseModel
-from typing import List
+import json
 from api.v1.api import router as api_router
 
 
-
-app = FastAPI()
-
-app.include_router(api_router, prefix="/api/v1")
-
-
-@app.get("/",  tags=["Endpoint Test"])
-def main_endpoint_test():
-    return {"message": "Welcome CI/CD Pipeline with GitHub Actions!"}
-
-
-@app.get("/user",  tags=["Endpoint Test"])
-def main_endpoint_test():
-    return {"message": "Welcome CI/CD Pipeline with GitHub Actions!"}
-
-def handler(event, context):
-    event['requestContext'] = {}  # Adds a dummy field; mangum will process this fine  
-    asgi_handler = Mangum(app)
-    response = asgi_handler(event, context)
-    return response
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# from fastapi import FastAPI, status
+# from app.api.api_v1.api import router as api_router
 # from mangum import Mangum
-# from pymongo import MongoClient
-# # from pydantic import BaseModel
-# from typing import List
-# import urllib.parse
-# import json
-# from api.v1.api import router as api_router
-# # password="root123"
-# # username="barkha"
-# # client = MongoClient(
-# #     "mongodb+srv://"+urllib.parse.quote_plus(username)+":"+urllib.parse.quote_plus(password)+"@cluster0.otmoo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
-# # )
-# # db = client.get_database('myFirstDatabase')
-
-
-# app = FastAPI()
-
-# app.include_router(api_router, prefix="/api/v1")
-
-# DB = "myFirstDatabase"
-# COLLECTION = "users"
-
-# # Message class defined in Pydantic
-# # class User(BaseModel):
-# #     first_name: str
-# #     last_name: str
-# #     email: str
-# #     mobile_number: int
-
-# @app.get("/",  tags=["Endpoint Test"])
-# def main_endpoint_test():
-#     return {"message": "Welcome CI/CD Pipeline with GitHub Actions!"}
-
-# def handler(event, context):
-#     event['requestContext'] = {}  # Adds a dummy field; mangum will process this fine  
-#     asgi_handler = Mangum(app)
-#     response = asgi_handler(event, context)
-#     return response
-
-# adding user
-# @app.post("/user", status_code=status.HTTP_201_CREATED)
-# def post_message(user: User):
-#         collection = db["users"] 
-#         print(collection)
-#         result = collection.insert_one(user.dict())
-#         print(result)
-#         return {"message":"Data inserted successfully"}
-
-# getting all users
-# @app.get("/user", response_model=List[User])
-# def get_users():
-#         collection = db["users"] 
-#         user_list = collection.find()
-#         users=[]
-#         for i in user_list:
-#             users.append(User(**i))
-#         print(users)
-#         return users
-
-# # getting single user by id
-# @app.get("/user/{user_id}", response_model=List[User])
-# def get_users():
-#         collection = db["users"] 
-#         user = collection.find()
-#         users=[]
-#         for i in user_list:
-#             users.append(User(**i))
-#         print(users)
-#         return users
-
-
-
-# from fastapi import FastAPI
-# from mangum import Mangum
-# import json
-# from api.v1.api import router as api_router
-
-
-# # from app.api.api_v1.api import router as api_router
-# # from mangum import Mangum
 
 # app = FastAPI(title='Serverless Lambda FastAPI')
 
-# app = FastAPI()
-# app.include_router(api_router, prefix="/api/v1")
+app = FastAPI()
 
-# @app.get("/",  tags=["Endpoint Test"])
-# def main_endpoint_test():
-#     return {"message": "Welcome CI/CD Pipeline with GitHub Actions!"}
 
-# @app.get("/user")
-# def main_endpoint_test():
-#     return {"message": "Welcome CI/CD Pipeline with GitHub Actions Post!"}
+@app.get("/",tags=["Endpoint Test"])
+async def root():
+    return {"message": "Hello World!"}
+
+@app.get("/user")
+def main_endpoint_test():
+    return {"message": "Welcome CI/CD Pipeline with GitHub Actions Post!"}
 
   
-# def handler(event, context):
-#     event['requestContext'] = {}  # Adds a dummy field; mangum will process this fine  
-#     asgi_handler = Mangum(app)
-#     response = asgi_handler(event, context)
-#     return response
+
+app.include_router(api_router, prefix="/api/v1")
+handler = Mangum(app)
+
+
+
+def handler(event, context):
+    event['requestContext'] = {}  # Adds a dummy field; mangum will process this fine
+    asgi_handler = Mangum(app)
+    response = asgi_handler(event, context)
+    json_data = event["queryStringParameters"] 
+    # user = json_data["user"]
+    return response
 
