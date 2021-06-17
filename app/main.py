603084925133b40a2951866becbeec1c09ad2c13
Adarsh Tiwari <Adarsh.tiwari@yash.com>
2021-06-17 11:20:40 +0530

    main change

--- app/main.py
+++ app/main.py
@@ -1,18 +1,17 @@
-
 from fastapi import FastAPI, status
 from mangum import Mangum
 from pymongo import MongoClient
-# from pydantic import BaseModel
+from pydantic import BaseModel
 from typing import List
 import urllib.parse
 import json
 from api.v1.api import router as api_router
-# password="root123"
-# username="barkha"
-# client = MongoClient(
-#     "mongodb+srv://"+urllib.parse.quote_plus(username)+":"+urllib.parse.quote_plus(password)+"@cluster0.otmoo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
-# )
-# db = client.get_database('myFirstDatabase')
+password="root123"
+username="barkha"
+client = MongoClient(
+    "mongodb+srv://"+urllib.parse.quote_plus(username)+":"+urllib.parse.quote_plus(password)+"@cluster0.otmoo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
+)
+db = client.get_database('myFirstDatabase')
 
 
 app = FastAPI()
@@ -23,11 +22,11 @@ DB = "myFirstDatabase"
 COLLECTION = "users"
 
 # Message class defined in Pydantic
-# class User(BaseModel):
-#     first_name: str
-#     last_name: str
-#     email: str
-#     mobile_number: int
+class User(BaseModel):
+    first_name: str
+    last_name: str
+    email: str
+    mobile_number: int
 
 @app.get("/",  tags=["Endpoint Test"])
 def main_endpoint_test():
@@ -38,6 +37,62 @@ def handler(event, context):
     asgi_handler = Mangum(app)
     response = asgi_handler(event, context)
     return response
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from fastapi import FastAPI, status
+# from mangum import Mangum
+# from pymongo import MongoClient
+# # from pydantic import BaseModel
+# from typing import List
+# import urllib.parse
+# import json
+# from api.v1.api import router as api_router
+# # password="root123"
+# # username="barkha"
+# # client = MongoClient(
+# #     "mongodb+srv://"+urllib.parse.quote_plus(username)+":"+urllib.parse.quote_plus(password)+"@cluster0.otmoo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
+# # )
+# # db = client.get_database('myFirstDatabase')
+
+
+# app = FastAPI()
+
+# app.include_router(api_router, prefix="/api/v1")
+
+# DB = "myFirstDatabase"
+# COLLECTION = "users"
+
+# # Message class defined in Pydantic
+# # class User(BaseModel):
+# #     first_name: str
+# #     last_name: str
+# #     email: str
+# #     mobile_number: int
+
+# @app.get("/",  tags=["Endpoint Test"])
+# def main_endpoint_test():
+#     return {"message": "Welcome CI/CD Pipeline with GitHub Actions!"}
+
+# def handler(event, context):
+#     event['requestContext'] = {}  # Adds a dummy field; mangum will process this fine  
+#     asgi_handler = Mangum(app)
+#     response = asgi_handler(event, context)
+#     return response
 
 # adding user
 # @app.post("/user", status_code=status.HTTP_201_CREATED)
