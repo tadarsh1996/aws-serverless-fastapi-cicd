0b879e0b08d6081043128c49f8d6655b31ca467a
Adarsh Tiwari <Adarsh.tiwari@yash.com>
2021-06-17 12:31:22 +0530

    same

--- app/main.py
+++ app/main.py
@@ -3,30 +3,14 @@ from mangum import Mangum
 from pymongo import MongoClient
 from pydantic import BaseModel
 from typing import List
-import urllib.parse
-import json
 from api.v1.api import router as api_router
-password="root123"
-username="barkha"
-client = MongoClient(
-    "mongodb+srv://"+urllib.parse.quote_plus(username)+":"+urllib.parse.quote_plus(password)+"@cluster0.otmoo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
-)
-db = client.get_database('myFirstDatabase')
+
 
 
 app = FastAPI()
 
 app.include_router(api_router, prefix="/api/v1")
 
-DB = "myFirstDatabase"
-COLLECTION = "users"
-
-# Message class defined in Pydantic
-class User(BaseModel):
-    first_name: str
-    last_name: str
-    email: str
-    mobile_number: int
 
 @app.get("/",  tags=["Endpoint Test"])
 def main_endpoint_test():
